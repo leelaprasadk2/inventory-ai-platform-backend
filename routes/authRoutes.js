768c5f65ef3bc84c01e2e4594212d239759619c3
klp <klpklpklp121212@gmail.com>
2026-06-01 21:04:19 +0530

    gmail brevo1 test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,6 +11,8 @@ import {
 
 } from "../controllers/authController.js";
 
+import sendEmail from "../utils/sendEmail.js";
+
 const router = express.Router();
 
 
@@ -88,6 +90,58 @@ router.post(
   "/google",
 
   googleLogin
+
+);
+
+// =========================
+// TEST EMAIL
+// =========================
+
+router.get(
+
+  "/test-email",
+
+  async (req, res) => {
+
+    try {
+
+      await sendEmail(
+
+        "YOUR_PERSONAL_EMAIL@gmail.com",
+
+        "Brevo Test",
+
+        "<h1>Email Working Successfully</h1>"
+
+      );
+
+      res.status(200).json({
+
+        success: true,
+
+        message: "Email sent"
+
+      });
+
+    }
+
+    catch (error) {
+
+      console.log("TEST EMAIL ERROR");
+
+      console.log(error);
+
+      res.status(500).json({
+
+        success: false,
+
+        message: error.message
+
+      });
+
+    }
+
+  }
 
 );
 
