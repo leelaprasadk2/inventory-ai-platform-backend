38a75e1ac202359ae0193c93548ba9f6e8cecc70
klp <klpklpklp121212@gmail.com>
2026-06-01 21:36:25 +0530

    gmail brevo1 test3

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,52 +1,40 @@
 import nodemailer from "nodemailer";
 
-
 console.log("SMTP HOST:", "smtp-relay.brevo.com");
-console.log("SMTP USER:", process.env.BREVO_EMAIL);
+console.log("SMTP USER:", process.env.BREVO_SMTP_LOGIN);
 console.log(
   "SMTP KEY EXISTS:",
   !!process.env.BREVO_SMTP_KEY
 );
 
 const transporter = nodemailer.createTransport({
-
   host: "smtp-relay.brevo.com",
-
   port: 465,
-
-  secure: false,
+  secure: true, // IMPORTANT
 
   auth: {
-
     user: process.env.BREVO_SMTP_LOGIN,
-
     pass: process.env.BREVO_SMTP_KEY
-
   }
-
 });
 
-const sendEmail = async (
-  email,
-  subject,
-  html
-) => {
+const sendEmail = async (email, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Inventory AI" <${process.env.BREVO_EMAIL}>`,
+      to: email,
+      subject,
+      html
+    });
 
-  const info = await transporter.sendMail({
+    console.log("✅ Email Sent:", info.messageId);
+    return info;
 
-    from: `"Inventory AI" <${process.env.BREVO_EMAIL}>`,
-
-    to: email,
-
-    subject,
-
-    html
-
-  });
-
-  console.log("✅ Email Sent:", info.messageId);
-
-  return info;
+  } catch (error) {
+    console.log("❌ EMAIL ERROR");
+    console.log(error);
+    throw error;
+  }
 };
 
 export default sendEmail;
